098d63f16993dbca80427d853cb4da920c64a230
prakash-infocusp <prakash@infocusp.com>
2024-11-22 05:41:39 +0000

    saving progress#10

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React from "react";
 import styles from "./Product.module.css";
-import productOne from "./images/product-one.svg"
+import productOne from "./images/product-one.svg";
+import productTwo from "./images/product-two.svg";
+import productThree from "./images/product-three.svg";
+import productFour from "./images/product-four.svg";
+import productFive from "./images/product-five.svg";
+import productSix from "./images/product-six.svg";
+import productSeven from "./images/product-seven.svg";
 
 const Product = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.productContainer}>
-            <img src={productOne} alt="apple tv 4k"/>
-            <span>
-                Apple TV 4k
-            </span>
-        </div>
+      <div className={styles.productContainer}>
+        <img src={productOne} alt="apple tv 4k" height={50}/>
+        <span>Apple TV 4k</span>
+      </div>
+      <div className={styles.productContainer}>
+        <img src={productTwo} alt="apple tv app" height={50}/>
+        <span>Apple TV app</span>
+      </div>
+      <div className={styles.productContainer}>
+        <img src={productThree} alt="apple tv plus" height={50}/>
+        <span>Apple TV+</span>
+      </div>
+      <div className={styles.productContainer}>
+        <img src={productFour} alt="homepod" height={50}/>
+        <span>HomePod</span>
+      </div>
+      <div className={styles.productContainer}>
+        <img src={productFive} alt="homepod mini" height={50}/>
+        <span>HomePod mini</span>
+      </div>
+      <div className={styles.productContainer}>
+        <img src={productSix} alt="home app" height={50}/>
+        <span>Home app</span>
+      </div>
+      <div className={styles.productContainer}>
+        <img src={productSeven} alt="accessories" height={50}/>
+        <span>Accessories</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- src/pages/tv-home.jsx
+++ src/pages/tv-home.jsx
@@ -4,9 +4,7 @@ import Product from "../../components/Product";
 const TVHOME = () => {
   return (
     <>
-      <h1>TV HOME Page</h1>
-      <p>This page is currently under construction. Please check back later.</p>
-      <Product
+      <Product/>
     </>
   );
 };
